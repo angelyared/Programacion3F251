957c3c82ee12885f5028e138d6515bf776565c1d
angelyared <a24110190@live.ceti.mx>
2025-04-04 02:07:41 +0000

    practica 2

---
+++ Include/Foco.hpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "EstadoFoco.hpp"
+
+class Foco  
+{
+private:
+    EstadoFoco estado;
+public:
+    Foco() {
+        estado = EstadoFoco ::Apagado;
+    } 
+    ~Foco() {}
+    void Encender (){
+        estado = EstadoFoco :: Encendido;
+    }
+    void Apagar (){
+        estado = EstadoFoco :: Apagado;
+    }
+    void Imprimir () {
+        switch (estado)
+        {
+        case EstadoFoco :: Encendido:
+            std::cout << "*";
+            break;
+        case EstadoFoco :: Apagado:
+            std::cout << "o";
+            break;
+        
+        default: throw "Estado foco no identificado";
+            break;
+        }
+    }
+    void Alternar () {
+        if (estado == EstadoFoco::Encendido)
+        {
+           estado = EstadoFoco::Apagado;            /* code */
+        }else{
+        estado = EstadoFoco::Encendido;
+        } 
+    }
+};
