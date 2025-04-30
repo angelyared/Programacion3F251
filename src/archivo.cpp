3f13150f8c1b6980988f8756cd8efdc7780ce2b1
angelyared <a24110190@live.ceti.mx>
2025-04-30 00:19:15 +0000

    practica 5

--- src/archivo.cpp
+++ src/archivo.cpp
@@ -3,6 +3,12 @@
 #include<string>
 using namespace std;
 
+struct Persona
+{
+    char nombre[10];
+    int edad;
+};
+
 int main(int argc, char const *argv[])
 {
     ifstream archivoEntrada("archivo.txt");
@@ -18,5 +24,23 @@ int main(int argc, char const *argv[])
     }
     
     archivoEntrada.close();
+
+    //
+    ofstream archivoSalidaBinario ("archivo.bin", ios::binary);
+    if(!archivoSalidaBinario){
+        cerr << "Error al leer archivo"<< endl;
+        return 1; //Finalizar con error
+    }
+
+    Persona p1 = {"Juan", 20};
+    Persona p2 = {"Pedro", 30};
+    Persona p2 = {"Maria", 40};
+
+    
+    archivoSalidaBinario.write((char*)(&p1), sizeof(Persona));
+    archivoSalidaBinario.write((char*)(&p2), sizeof(Persona));
+    archivoSalidaBinario.write((char*)(&p3), sizeof(Persona));
+
+    return 0;
 
 }
